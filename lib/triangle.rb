4fb2e981401d6a9553508b3711b3cfce8b387bdb
alpbourne <alpbourne@gmail.com>
2017-10-04 16:04:14 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,6 +11,8 @@ class Triangle
   def kind
     if l1 < 0 || l2 < 0 || l3 < 0 || l1 + l2 <= l3 || l2 + l3 <= l1|| l3 + l1 <= l2
       raise TriangleError
+    else l1 != l2 && l1 != l3 && l2 != l3
+      :scalene
 
   end
 
